52a60dd9e210e1cfa1497bbc0f09a5b6ad9ddd2d
KINARI NISHIYAMA <KINARI@nishiyamaseisei-no-MacBook-Air.local>
2015-04-16 20:11:37 +0900

    Initial Commit

---
+++ TousanTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TousanTimer
+//
+//  Created by KINARI NISHIYAMA on 2015/04/16.
+//  Copyright (c) 2015年 KINARI NISHIYAMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
